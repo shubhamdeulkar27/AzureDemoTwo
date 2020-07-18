623677065fe33a4140c3199affcd56b89af73ae5
shubham <shubhamdeulkar27@gmail.com>
2020-07-18 10:37:35 +0530

    Added: Third Commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace AzureDemoTwo.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+
         }
 
         public IActionResult Index()
